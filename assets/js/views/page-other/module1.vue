3f7532495ad56495dc55eec0d83c75804d8ae7b4
duyang <79229788@qq.com>
2017-06-10 13:07:21 +0800

    update

---
+++ assets/js/views/page-other/module1.vue
@@ -0,0 +1,43 @@
+<style lang="scss">
+  @import "../../../styles/source.scss";
+  #module{
+
+  }
+</style>
+
+<template>
+  <section id="module1">
+    <p>this is module1 content form other page</p>
+    <p>timer: {{ count }}</p>
+  </section>
+</template>
+
+<script>
+  import Vue from 'vue';
+
+  const sharedEvent = app.page.shares.otherEvent;
+  const sharedState = app.page.shares.otherStore.state;
+  export default {
+    data: function () {
+      return {
+        sharedState: sharedState,
+        count: 0
+      }
+    },
+    created: function () {
+      setInterval(() => {
+        this.count++;
+      }, 1000);
+    },
+    mounted: function () {},
+    methods: {
+      //*********************************内置方法
+      //***************************
+
+
+      //*********************************内置事件
+      //***************************
+
+    }
+  }
+</script>
