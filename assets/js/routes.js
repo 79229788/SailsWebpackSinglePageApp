caee1c81bdfa0ccb278b7658dcded96127d52d4f
duyang <79229788@qq.com>
2017-06-11 11:38:21 +0800

    update

--- assets/js/routes.js
+++ assets/js/routes.js
@@ -3,8 +3,24 @@ import VueRouter from 'vue-router';
 Vue.use(VueRouter);
 
 const routes = [
-  { path: '/', name: 'home', component: r => require.ensure([], () => r(require('./views/page-home')), 'page-home') },
-  { path: '/other', name: 'other', component: r => require.ensure([], () => r(require('./views/page-other')), 'page-other') }
+  {
+    path: '/',
+    name: 'home',
+    component: function (resolve) {
+      require.ensure([], function () {
+        resolve(require('./views/page-home'));
+      }, 'page-home')
+    }
+  },
+  {
+    path: '/other',
+    name: 'other',
+    component: function (resolve) {
+      require.ensure([], function () {
+        resolve(require('./views/page-other'));
+      }, 'page-other')
+    }
+  },
 ];
 
 export default new VueRouter({
