0acd19e994d8f6efa0aba34ddbe1f73af0a14551
duyang <79229788@qq.com>
2017-06-12 14:32:47 +0800

    update

--- api/generator/page/templates/module.template.js
+++ api/generator/page/templates/module.template.js
@@ -25,12 +25,21 @@
     },
     created: function () {
       sharedStore.set<%=moduleFullCamelName%>(this);
+      this.initData();
     },
     mounted: function () {},
     methods: {
       //*********************************内置方法
       //***************************
+      //*********初始数据
+      initData: function () {
 
+      },
+      //*********重置数据
+      reset: function () {
+
+        this.initData();
+      },
 
       //*********************************内置事件
       //***************************
