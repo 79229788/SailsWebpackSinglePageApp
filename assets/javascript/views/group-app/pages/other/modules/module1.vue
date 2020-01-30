2a63708f9d4c3337ccb26dbcf27a7833b7a9818f
duyang <79229788@qq.com>
2020-01-30 11:45:52 +0800

    update

--- assets/javascript/views/group-app/pages/other/modules/module1.vue
+++ assets/javascript/views/group-app/pages/other/modules/module1.vue
@@ -25,9 +25,11 @@
       }
     },
     created: function () {
-      setInterval(() => {
-        this.count++;
-      }, 1000);
+      (async () => {
+        setInterval(() => {
+          this.count++;
+        }, 1000);
+      })();
     },
     mounted: function () {},
     methods: {
